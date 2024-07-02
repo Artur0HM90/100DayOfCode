c97c7f6b4becc2be023a165ad487e02cb6571208
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-07-01 23:32:19 -0500

    Par_Impar en JavaScript

---
+++ JavaScript/par_impar.js
@@ -0,0 +1,9 @@
+
+let ingresaNumero = prompt("Ingresa un número: ")
+
+if (ingresaNumero % 2 == 0) {
+    console.log("El número ingresado es PAR.")
+} else {
+    console.log("El número ingresado es IMPAR.")
+}
+
