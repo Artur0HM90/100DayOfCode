39f71c5b0da83a93c0d84adb63f1e7bdf640493a
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-07-02 22:01:55 -0500

    la suma de dos números - py

---
+++ Python/suma_de_dos_numeros.py
@@ -0,0 +1,6 @@
+primerNumero = int(input("Ingresa el primer Número: "))
+segundoNumero = int(input("Ingresa el segundo Número: "))
+
+resultado = primerNumero + segundoNumero
+
+print(f"La suma de {primerNumero} + {segundoNumero} = {resultado}")
