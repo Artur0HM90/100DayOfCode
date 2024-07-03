b838f2ccd8a4cd0b959acdb8bd36b54f7b7b6021
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-07-02 22:30:09 -0500

    suma de dos  números - js

---
+++ JavaScript/sumaDeDosNumeros.js
@@ -0,0 +1,8 @@
+let primerNumero, segundo, resultado
+
+primerNumero = parseFloat(prompt("Ingresa el primer número: "))
+segunsoNumero = parseFloat(prompt("Ingresa el segundo número: "))
+
+
+resultado = primerNumero + segunsoNumero
+console.log("La suma de " + primerNumero + " + " + segunsoNumero + " = " + resultado)
