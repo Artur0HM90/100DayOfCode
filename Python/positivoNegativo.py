f3ecdf045ef2c9b951a3499311e788451377a0a9
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-07-04 21:00:53 -0500

    Positivo, Negativo y cero - py

---
+++ Python/positivoNegativo.py
@@ -0,0 +1,10 @@
+ingreseNumero = float(input("Ingrese un número: "))
+
+if ingreseNumero > 0:
+    print("El número ingresado es positivo.")
+
+elif ingreseNumero < 0:
+    print("El número ingresado es negativo.")
+
+else:
+    print("El número ingresado es cero")
