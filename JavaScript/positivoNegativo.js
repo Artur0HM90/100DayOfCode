30511e1ec2ca485f7254d371fd0d6548e4588e99
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-07-04 20:54:32 -0500

    Positivi, Negativo y cero - js

---
+++ JavaScript/positivoNegativo.js
@@ -0,0 +1,9 @@
+let ingresaNumero = prompt("Ingresa un número: ")
+
+if (ingresaNumero > 0) {
+    console.log("El número ingresado es positivo.")
+} else if (ingresaNumero < 0) {
+    console.log("El número ingresado es negativo")
+} else {
+    console.log("El número ingresado es cero.")
+}
