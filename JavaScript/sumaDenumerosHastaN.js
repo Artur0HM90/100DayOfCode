c53a5322f375f61eff5b66234e10d67fd43876ec
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-07-05 23:42:08 -0500

    Suma de números ingresados - js

---
+++ JavaScript/sumaDenumerosHastaN.js
@@ -0,0 +1,8 @@
+let ingresaNumero = prompt("Ingresa un número: ")
+let suma = 0
+
+for (let index = 1; index <= ingresaNumero; index++) {
+    suma += index
+    console.log(`La suma del número ingresado es: ${suma}`)
+    
+}
