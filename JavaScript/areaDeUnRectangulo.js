3fed1f36786e26c6134c86c781b46a070a37513c
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-07-08 21:37:31 -0500

    Area de un rectangulo - js

---
+++ JavaScript/areaDeUnRectangulo.js
@@ -0,0 +1,7 @@
+let ingresaBase = parseFloat(prompt("Ingresa la base: "))
+let ingresaAltura = parseFloat(prompt("Ingresa la altura: "))
+let resultado = ingresaBase * ingresaAltura
+document.write(`El area del rectangulo es ${resultado}` )
+
+
+
