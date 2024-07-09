cbadbf54b6948a75e004ed07756841ac45f14f06
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-07-08 21:44:41 -0500

    Area de un rectangulo - py

---
+++ Python/areaDeUnRectangulo.py
@@ -0,0 +1,4 @@
+ingresaBase = float(input("Ingresa la base: "))
+ingresaAltura = float(input("Ingresa la altura: "))
+resultado = ingresaBase * ingresaAltura
+print(f"El area del rectangulo es {resultado}")
