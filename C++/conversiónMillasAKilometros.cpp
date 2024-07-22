21b87e44359c182b5b7106dcb592e6d6c28c844d
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-07-21 22:55:52 -0500

    conversion kilometroa a millas con funcines - c++

--- C++/conversiónMillasAKilometros.cpp
+++ C++/conversiónMillasAKilometros.cpp
@@ -3,13 +3,28 @@
 #include <iostream>
 using namespace std;
 
+double millas(double ingresarMillas)
+{
+    float valorKilometros = 1.60934;
+    cout << "ingresa millas: ";
+    cin >> ingresarMillas;
+    return ingresarMillas * valorKilometros;
+}
+
+double kilometros(double ingresaKilometrso)
+{
+    float valorMillas = 0.621371;
+    cout << "Imgresa kilometros: ";
+    cin >> ingresaKilometrso;
+    return ingresaKilometrso * valorMillas;
+}
+
 int main()
 {
-
     int opcion;
-    float millas, kilometros, resultado;
-    float valorKilometros = 1.60934;
-    float valorMillas = 0.621371;
+    float Millas, Kilometros, resultado;
+    /*float valorKilometros = 1.60934;*/
+    /*float valorMillas = 0.621371;*/
 
     cout << "1. Millas a Kilometros." << endl;
     cout << "2. Kilometros a Millas." << endl;
@@ -20,18 +35,26 @@ int main()
     {
     case 1:
         cout << "MILLAS A KILOMETROS." << endl;
+        resultado = millas(Millas);
+        cout << resultado;
+        /*
         cout << "Ingresa Millas: ";
         cin >> millas;
         resultado = millas * valorKilometros;
         cout << "Distancia a kilometros: " << resultado;
+        */
         break;
 
     case 2:
         cout << "KILOMETROS A MILLAS." << endl;
+        resultado = kilometros(Kilometros);
+        cout << resultado;
+        /*
         cout << "Ingresa kilometros: ";
         cin >> kilometros;
         resultado = kilometros * valorMillas;
         cout << "Distancia a Millas: " << resultado;
+        */
         break;
 
     default:
