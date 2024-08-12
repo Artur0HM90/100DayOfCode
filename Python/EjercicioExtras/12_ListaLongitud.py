465d7c70222efaef3ca585fb5b9f07efd6aaf5eb
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-11 21:52:54 -0500

    lista longitud - py

---
+++ Python/EjercicioExtras/12_ListaLongitud.py
@@ -0,0 +1,6 @@
+# Longitud de palabras: Dada una lista de palabras, imprime cada palabra seguida de su longitud.
+
+listaPalabras = ["Arturo", "Miguel", "Luis", "Reyna", "Elcecia", "Choloque"]
+
+for i in listaPalabras:
+    print(f"{i} - {len(i)} caracteres.")
