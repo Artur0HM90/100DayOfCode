fcfdfa36a4cf63bab7859874564a88e8475c709d
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-11 21:30:09 -0500

    lista de dias - py

---
+++ Python/EjercicioExtras/11_imprimirLista.py
@@ -0,0 +1,6 @@
+# Imprimir una lista: Dada una lista de nombres, usa un bucle for para imprimir cada nombre en la lista.
+
+listaDeDias = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+
+for i in listaDeDias:
+    print(i)
