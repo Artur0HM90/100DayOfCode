5602470be4087157a7ddaeeea8e3169fab6fdc81
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-11 21:25:21 -0500

    suma de una lista - py

---
+++ Python/EjercicioExtras/10_sumaLista.py
@@ -0,0 +1,8 @@
+# Suma de una lista de números: Dada una lista de números, utiliza un bucle for para calcular su suma.
+
+listaNumeros = [1,2,3,4,5,6,7,8,9,10]
+suma = 0
+
+for i in  listaNumeros:
+    suma += i
+print(f"La suma de la lista es: {suma}")
