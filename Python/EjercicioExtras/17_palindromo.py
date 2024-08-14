b3781942f6d2dd286a602afd5099689c33f11062
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-14 18:10:59 -0500

    mayor y menor - py

---
+++ Python/EjercicioExtras/17_palindromo.py
@@ -0,0 +1,9 @@
+# Mayor y menor de una lista: Encuentra el mayor y el menor número en una lista.
+
+listaNumeros = [4,5,6,20,5,18,21,2]
+
+maximo = max(listaNumeros)
+print(maximo)
+
+minimo = min(listaNumeros)
+print(minimo)
