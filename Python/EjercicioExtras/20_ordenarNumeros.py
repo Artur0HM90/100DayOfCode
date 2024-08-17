018abaae053f75ed4e0468108e9950ac2e9f65d2
HURTADO MÉNDEZ Arturo Miguel <arturohurtadomendez90@gmail.com>
2024-08-16 21:55:25 -0500

    lista ascendente y descendente con FOR - py

--- Python/EjercicioExtras/20_ordenarNumeros.py
+++ Python/EjercicioExtras/20_ordenarNumeros.py
@@ -1,18 +1,20 @@
 # Ordenar números: Ordena tres números en orden ascendente.
 
 ingreseLimite = int(input("Ingresa limite de ingreso: "))
+listaNumero = []
 
 for i in range (1, ingreseLimite + 1):
+    ingreseNumero = float(input(f"Ingrese {i}: "))
+    listaNumero.append(f"{ingreseNumero:.0f}")
 
+    # numeros = [4,5,1,6,0,2,4,3]
+    # print("Lista de números")
+    #print(f"{numeros}")
 
+print("Lista de números ordenadas.")
+listaNumero.sort()
+print(listaNumero)
 
-    numeros = [4,5,1,6,0,2,4,3]
-    print("Lista de números")
-    print(f"{numeros}")
-
-    print("Lista de números ordenadas.")
-    numeros.sort()
-    print(numeros)
-    print("Lista de números descendente.")
-    numeros.sort(reverse=True)
-    print(numeros)
+print("Lista de números descendente.")
+listaNumero.sort(reverse=True)
+print(listaNumero)
